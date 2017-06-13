6bea2e241b700fdd0b6cb22cfb13712ca302319b
Samuel Prevost <s@muel.coffee>
2017-06-13 19:32:10 +0200

    Added egg-info files and __main__ into __init__

--- vcard-splitter/__init__.py
+++ vcard-splitter/__init__.py
@@ -22,46 +22,50 @@ def isValidVCard(input_file):
 
     return False
 
-# sys.argv[0] is the file itself
-if len(sys.argv) < 3:
-    helpMessage()
-    exit()
+def main():
+    # sys.argv[0] is the file itself
+    if len(sys.argv) < 3:
+        helpMessage()
+        exit()
 
-input_file = sys.argv[1]
-if not isValidVCard(input_file):
-    print("The given file wasn't a valid vCard file !")
-    helpMessage()
-    exit()
+    input_file = sys.argv[1]
+    if not isValidVCard(input_file):
+        print("The given file wasn't a valid vCard file !")
+        helpMessage()
+        exit()
 
-output_dir = sys.argv[2]
-# E.g. output_dir = "/Users/sam1902/Downloads/CHUMontpellierVCF/out/"
-if output_dir[-1:] != '/':
-    output_dir = output_dir + "/"
+    output_dir = sys.argv[2]
+    # E.g. output_dir = "/Users/sam1902/Downloads/CHUMontpellierVCF/out/"
+    if output_dir[-1:] != '/':
+        output_dir = output_dir + "/"
 
-# E.g. output_seed = 'contacts-part-'
-if len(sys.argv) >= 4:
-    output_seed = sys.argv[3]
-else:
-    output_seed = ""
+    # E.g. output_seed = 'contacts-part-'
+    if len(sys.argv) >= 4:
+        output_seed = sys.argv[3]
+    else:
+        output_seed = ""
 
-with open(input_file, 'r') as f:
-    i = 0
-    individualContact = []
-    for line in f:
-        if ("BEGIN:VCARD" in line):
-            if i > 0:
-                with open(output_dir + output_seed + str(i) + '.vcf','w') as oFile:
-                    oFile.write(line)
-                    for line in individualContact:
-                        oFile.write(line) 
-                del individualContact[:]
-            i += 1
-        else:
-            individualContact.append(line)
+    with open(input_file, 'r') as f:
+        i = 0
+        individualContact = []
+        for line in f:
+            if ("BEGIN:VCARD" in line):
+                if i > 0:
+                    with open(output_dir + output_seed + str(i) + '.vcf','w') as oFile:
+                        oFile.write(line)
+                        for line in individualContact:
+                            oFile.write(line) 
+                    del individualContact[:]
+                i += 1
+            else:
+                individualContact.append(line)
 
-    with open(output_dir + output_seed + str(i) + '.vcf','w') as oFile:
-        oFile.write("BEGIN:VCARD\n")
-        for line in individualContact:
-            oFile.write(line) 
+        with open(output_dir + output_seed + str(i) + '.vcf','w') as oFile:
+            oFile.write("BEGIN:VCARD\n")
+            for line in individualContact:
+                oFile.write(line) 
 
-print("Done !")
+    print("Done !")
+
+if __name__ == "__main__":
+    main()
